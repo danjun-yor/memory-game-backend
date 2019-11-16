b346519dfcb3d22b02f9e0690ea9ae32a821f46a
Junyeol <jylee@rsupport.com>
2019-11-17 01:30:54 +0900

    signup

--- src/api/User/signUp/signUp.ts
+++ src/api/User/signUp/signUp.ts
@@ -1,19 +1,31 @@
 import { prisma } from "../../../../generated/prisma-client";
+import { APP_SECRET } from "../../../utils";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
-    logIn: async (_: any, args: any) => {
+    signUp: async (_: any, args: any) => {
       const { name, email, password } = args;
-      const exists = await prisma.$exists.user({ name });
+      const bcryptPassword = await bcrypt.hash(password, 10);
+
+      const exists = await prisma.$exists.user({ email });
       if (exists) {
-        throw Error("This username is already taken");
+        throw Error("이미 존재하는 이메일입니다.");
       }
-      await prisma.createUser({
+
+      const user = await prisma.createUser({
         name,
         email,
-        password
+        password: bcryptPassword
       });
-      return true;
+
+      const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+      return {
+        token,
+        user
+      };
     }
   }
 };
--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,10 @@ import jwt from "jsonwebtoken";
 
 const APP_SECRET = "awesome-memory-game";
 
-function getUserId() {
-  // const Authorization = request.get("Authorization");
+function getUser(token: string) {
+  const user = jwt.verify(token, APP_SECRET);
+
+  return user;
 }
+
+export { APP_SECRET, getUser };
