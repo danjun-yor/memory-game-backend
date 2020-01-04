7a94e4a54a8703b58ec25059ea36c976d93f5479
Junyeol <jylee@rsupport.com>
2020-01-04 17:35:29 +0900

    saveMyRecord

---
+++ src/api/User/saveMyRecord/saveMyRecord.ts
@@ -0,0 +1,40 @@
+import { prisma } from "../../../../generated/prisma-client";
+import { ResolverFn } from "apollo-server";
+
+export default {
+  Mutation: {
+    saveMyRecord: async (
+      _: any,
+      { score, stage, playTime }: any,
+      context: any,
+      info: any
+    ) => {
+      try {
+        const userExists = await prisma.$exists.user({
+          id: context.id
+        });
+
+        if (!userExists) {
+          throw new Error("You need to be authenticated.");
+        }
+
+        await prisma.updateUser({
+          data: {
+            records: {
+              create: {
+                score,
+                stage,
+                playTime
+              }
+            }
+          },
+          where: { id: context.id }
+        });
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
+  }
+};
